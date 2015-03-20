79f5116765e6cc29949e2b25142f38493be3d066
figmentbml <beth.jaswa@gmail.com>
2015-03-20 15:19:53 -0600

    don't need to be logged in to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  skip_before_action :require_login, only: [:index, :show]
+  skip_before_action :require_login, only: [:index, :show, :create]
 
   def index
     render json: User.all
